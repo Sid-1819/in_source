fff9aaeb19625c433d33a4a4c43e86847988488c
siddhesh.shirdhankar <siddhesh.shirdhankar@cmssl.in>
2024-11-13 19:12:09 +0530

    feat:added sub tabs in info

--- src/app/contest/[id]/_components/information/page.tsx
+++ src/app/contest/[id]/_components/information/page.tsx
@@ -2,6 +2,9 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from '~/components/ui/tabs';
 import React from 'react'
 import ApplicantsList from '../participants/page';
 import WinnersList from '../winners/page';
+import RulesTab from './rules-tab/page';
+import GuidelinesTab from './guidelines-tab/page';
+import MoreInfoTab from './more-info/page';
 
 const InformationPage = () => {
     return (
@@ -9,15 +12,15 @@ const InformationPage = () => {
           <Tabs defaultValue="description" className="w-full">
             <TabsList className="grid w-full grid-cols-3 mb-8">
              
-              <TabsTrigger value="info" className="flex items-center gap-2">
+              <TabsTrigger value="rules" className="flex items-center gap-2">
                 {/* <Info className="h-4 w-4" /> */}
                 <span className="hidden md:inline">Rules</span>
               </TabsTrigger>
-              <TabsTrigger value="participants" className="flex items-center gap-2">
+              <TabsTrigger value="guidelines" className="flex items-center gap-2">
                 {/* <Users className="h-4 w-4" /> */}
                 <span className="hidden md:inline">Guidelines</span>
               </TabsTrigger>
-              <TabsTrigger value="winners" className="flex items-center gap-2">
+              <TabsTrigger value="more-info" className="flex items-center gap-2">
                 {/* <Trophy className="h-4 w-4" /> */}
                 <span className="hidden md:inline">More Info</span>
               </TabsTrigger>
@@ -27,12 +30,15 @@ const InformationPage = () => {
   
           
   
-          <TabsContent value="participants">
-            <ApplicantsList/>
+          <TabsContent value="rules">
+           <RulesTab/>
           </TabsContent>
   
-          <TabsContent value="winners">
-            <WinnersList />
+          <TabsContent value="guidelines">
+         <GuidelinesTab/>
+          </TabsContent>
+          <TabsContent value="more-info">
+         <MoreInfoTab/>
           </TabsContent>
           </Tabs>
         </div>
