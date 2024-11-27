9dd8a5a81ad826b42a137e418f907602e575fcb5
swatantra.yadav <swatantra.yadav@cmssl.in>
2024-11-27 12:27:36 +0530

    chore: Change Project Structure

--- src/app/(root)/contest/[id]/page.tsx
+++ src/app/(root)/contest/[id]/page.tsx
@@ -1,11 +1,11 @@
 
 import React from "react";
-
-import ContestDetailsContent from "./_components/contest-details-context";
+import ContestDetailsContent from "./contest-details-context";
 
 async function Page({ params }: { params: Promise<{ id: string }> }) {
 
-    const id = (await params).id
+    // const id = (await params).id
+    const { id } = await params;
     return (
         // make a async db call
         <ContestDetailsContent id={id} />
